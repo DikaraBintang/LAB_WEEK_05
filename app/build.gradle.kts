6609c2c3cb6fa846c9f5bef13dfc830e4942b2b4
Kairose8 <hafizhdikara@gmail.com>
2025-10-05 14:30:25 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    
+
 
 
     implementation(libs.androidx.core.ktx)
